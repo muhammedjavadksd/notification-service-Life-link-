855b4664329f1f90befa96668760fbd97649bc1a
muhammedjavadksd <javadjazz899@gmail>
2024-10-05 12:04:31 +0530

    close ticket timing

--- communication/Consumer/User/TicketConsumer.js
+++ communication/Consumer/User/TicketConsumer.js
@@ -1,6 +1,7 @@
 const const_data = require("../../../config/const_data");
 const mailTemplate = require("../../../config/template/mailTemplate");
 const { communicationConnection } = require("../../config");
+const nodeMailer = require("nodemailer");
 
 
 const TicketNotification = {
@@ -14,33 +15,38 @@ const TicketNotification = {
             await channel.consume(TICKET_CLOSE_NOTIFICATION, (message) => {
 
                 if (message) {
+                    const bulkEmail = [];
                     const data = JSON.parse(message.content.toString())
-                    const { email, name, ticket_id, title } = data;
+                    for (let each = 0; each < data.length; each++) {
+                        const { email, name, ticket_id, title } = data[each];
+                        console.log(data);
 
-                    const mailContent = mailTemplate.ticketClosedMail(name, ticket_id, title);
+                        const mailContent = mailTemplate.ticketClosedMail(name, ticket_id, title);
 
-                    const mailTransport = nodeMailer.createTransport({
-                        service: const_data.MAIL_CONFIG.service,
-                        auth: const_data.MAIL_CONFIG.auth
-                    })
-
-                    const mailOption = {
-                        from: const_data.MAIL_CONFIG.auth.user,
-                        to: email,
-                        subject: 'Ticket Closed Notification!',
-                        html: mailContent
-                    };
-
-                    return new Promise((resolve, reject) => {
-                        mailTransport.sendMail(mailOption).then(() => {
-                            console.log("Ticket close mail has been sent")
-                        }).catch((err) => {
-                            console.log("Ticket close mail has been sent")
-                            console.log(err)
+                        const mailTransport = nodeMailer.createTransport({
+                            service: const_data.MAIL_CONFIG.service,
+                            auth: const_data.MAIL_CONFIG.auth
                         })
+
+                        const mailOption = {
+                            from: const_data.MAIL_CONFIG.auth.user,
+                            to: email,
+                            subject: 'Ticket Closed Notification!',
+                            html: mailContent
+                        };
+
+                        console.log(mailOption);
+                        bulkEmail.push(mailTransport.sendMail(mailOption))
+                    }
+
+
+                    return Promise.all(bulkEmail).then(() => {
+                        console.log("Ticket close mail has been sent")
+                    }).catch((err) => {
+                        console.log("Ticket close mail has been sent")
+                        console.log(err)
                     })
                 }
-
             }, { noAck: true })
         } catch (e) {
             console.log("Something went wrong");
@@ -57,32 +63,38 @@ const TicketNotification = {
 
                 if (message) {
                     const data = JSON.parse(message.content.toString())
-                    const { email, name, ticket_id, title, close_date } = data;
 
-                    const mailContent = mailTemplate.ticketClosedMail(name, ticket_id, title);
+                    const bulkMail = []
 
-                    const mailTransport = nodeMailer.createTransport({
-                        service: const_data.MAIL_CONFIG.service,
-                        auth: const_data.MAIL_CONFIG.auth
-                    })
+                    for (let each = 0; each < data.length; each++) {
 
-                    const mailOption = {
-                        from: const_data.MAIL_CONFIG.auth.user,
-                        to: email,
-                        subject: 'Ticket Closed Notification!',
-                        html: mailContent
-                    };
 
-                    return new Promise((resolve, reject) => {
-                        mailTransport.sendMail(mailOption).then(() => {
-                            console.log("Ticket close mail has been sent")
-                        }).catch((err) => {
-                            console.log("Ticket close mail has been sent")
-                            console.log(err)
+                        const { email, name, ticket_id, title, close_date } = data[each];
+
+                        const mailContent = mailTemplate.ticketClosedWarningMail(name, ticket_id, title, close_date);
+
+                        const mailTransport = nodeMailer.createTransport({
+                            service: const_data.MAIL_CONFIG.service,
+                            auth: const_data.MAIL_CONFIG.auth
                         })
+
+                        const mailOption = {
+                            from: const_data.MAIL_CONFIG.auth.user,
+                            to: email,
+                            subject: 'Ticket Closed Notification!',
+                            html: mailContent
+                        };
+
+                        bulkMail.push(mailTransport.sendMail(mailOption))
+                    }
+
+                    Promise.all(bulkMail).then(() => {
+                        console.log("Ticket close mail has been sent")
+                    }).catch((err) => {
+                        console.log("Ticket close mail has been sent")
+                        console.log(err)
                     })
                 }
-
             }, { noAck: true })
         } catch (e) {
             console.log("Something went wrong");
